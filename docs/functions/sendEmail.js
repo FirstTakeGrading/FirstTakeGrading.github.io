1b9a496414822b6c436e6f636d90138197421934
Noah List <noah2list@gmail.com>
2022-01-18 15:32:40 -0600

    pushes

--- docs/functions/sendEmail.js
+++ docs/functions/sendEmail.js
@@ -2,7 +2,7 @@ function sendEmail() {
     Email.send({
         Host: "smtp.gmail.com",
         Username: "noah2list@gmail.com",
-        Password: "fireb",
+        Password: "firebirds2",
         To: 'noah@noahlist.com',
         From: "First Take",
         Subject: "Welcome to First Take!",
@@ -12,4 +12,22 @@ function sendEmail() {
         });
 }
 
-// Add MySQL help...
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
